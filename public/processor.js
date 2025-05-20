18ff9fca4e83a220c2964a41e29374e2b348b92a
SoumyaNC <soumya.nc@gmail.com>
2025-05-20 16:42:51 +0530

    Add files via upload

--- public/processor.js
+++ public/processor.js
@@ -1,27 +1,16 @@
-
-
 // public/processor.js
 
-class AudioCaptureProcessor extends AudioWorkletProcessor {
-  constructor() {
-    super();
-    this.buffer = [];
-  }
-
+registerProcessor('audio-capture', class extends AudioWorkletProcessor {
   process(inputs) {
-    const input = inputs[0][0];
-    if (input) {
-      const floatData = new Float32Array(input);
-      const int16Data = new Int16Array(floatData.length);
-      for (let i = 0; i < floatData.length; i++) {
-        int16Data[i] = floatData[i] * 0x7FFF;
+    const input = inputs[0];
+    if (input && input[0]) {
+      const float32 = input[0];
+      const int16 = new Int16Array(float32.length);
+      for (let i = 0; i < float32.length; i++) {
+        int16[i] = Math.max(-32768, Math.min(32767, float32[i] * 32768));
       }
-      //this.port.postMessage(int16Data.buffer);
-      this.port.postMessage(Int16Array.from(input.map(v => v * 32767)).buffer);
-
+      this.port.postMessage(int16.buffer, [int16.buffer]);
     }
     return true;
   }
-}
-
-registerProcessor('audio-capture', AudioCaptureProcessor);
+});
